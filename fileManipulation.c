c031a7f6bac7cd498befd2ba4919ef9f9cee2951
Lutfil Hadi Mohd Hafez <61901435+lutfilhadimohdafez@users.noreply.github.com>
2020-11-09 03:30:21 +0800

    adding delay function

--- fileManipulation.c
+++ fileManipulation.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 /*1.Construct a database system that contains name and identity card (IC).
 This system should have 4 functions that are
 addRecord, editRecord, deleteRecord, searchByName, searchByIC.
 The name of database file is record.csv. 
 */
 
+// Function prototype untuk user define functions
 void mainMenu();
 void readRecord();
+int editRecord();
+int addRecord();
+int deleteRecord();
+int searchByName();
+int searchByIC();
 
+
+//main Program function
 int main()
 {
     //initializing the program loop variable, default set as Y(yes)
@@ -49,22 +58,28 @@ void mainMenu()
     case 1:
         //Read record
         printf("\nSelected choice:%d, doing %s\n", userInput,menuChoice[userInput+1]);
+        delay(3);
+        readRecord();
         break;
     case 2:
         //add record
         printf("\nSelected choice:%d, doing %s\n", userInput,menuChoice[userInput+1]);
+        delay(3);
         break;
     case 3:
         //edit record
         printf("\nSelected choice:%d, doing %s\n", userInput,menuChoice[userInput+1]);
+        delay(3);
         break;
     case 4:
         //delete record
         printf("\nSelected choice:%d, doing %s\n", userInput,menuChoice[userInput+1]);
+        delay(3);
         break;
     case 5:
         //search record
         printf("\nSelected choice:%d, doing %s\n", userInput,menuChoice[userInput+1]);
+        delay(3);
     default:
         printf("\nDefault: Value is: %d", userInput);
     }
@@ -85,6 +100,7 @@ void readRecord()
         {
             puts(buffer);
             /* Some processing */
+            delay(1);
         }
         fclose(studentRecord);
     }
@@ -114,3 +130,16 @@ int searchByIC()
 {
     return 0;
 }
+
+int delay(int number_of_seconds) 
+{ 
+    // Converting time into milli_seconds 
+    int milli_seconds = 1000 * number_of_seconds; 
+  
+    // Storing start time 
+    clock_t start_time = clock(); 
+  
+    // looping till required time is not achieved 
+    while (clock() < start_time + milli_seconds) 
+        ; 
+} 
