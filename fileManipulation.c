b7c0762d588269fe1d3d12751dcc0b00b5870707
Lutfil Hadi Mohd Hafez <61901435+lutfilhadimohdafez@users.noreply.github.com>
2020-11-08 23:25:16 +0800

    putting on functions and initialization of reading csv file

--- fileManipulation.c
+++ fileManipulation.c
@@ -1,5 +1,47 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/*1.Construct a database system that contains name and identity card (IC).
+This system should have 4 functions that are
+addRecord, editRecord, deleteRecord, searchByName, searchByIC.
+The name of database file is record.csv. 
+*/
+int main()
+{
+    //initialization of program
+    printf("File manipulation program.","r");
 
-int main(){
+    //initialization of file variables
+    FILE *studentRecord;
+    studentRecord = fopen("C:\Users\Lutfil\Documents\CproFileManipulation");
+    fprintf(studentRecord, "%s %s %s %d", "We", "are", "in", 2012);
+
+    fclose(studentRecord);
+
+    return 0;
+}
+
+int addRecord()
+{
+    return 0;
+}
+
+int editRecord()
+{
+    return 0;
+}
+
+int deleteRecord()
+{
+    return 0;
+}
+
+int searchByName()
+{
+    return 0;
+}
+
+int searchByIC()
+{
     return 0;
 }
