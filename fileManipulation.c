6d09b79efee8e55a76cdd98cd094bf5d01b89abe
Lutfil Hadi Mohd Hafez <61901435+lutfilhadimohdafez@users.noreply.github.com>
2020-11-09 01:06:59 +0800

    creating menu interface for  program

--- fileManipulation.c
+++ fileManipulation.c
@@ -7,19 +7,49 @@ addRecord, editRecord, deleteRecord, searchByName, searchByIC.
 The name of database file is record.csv. 
 */
 void readRecord();
+void mainMenu();
 
 int main()
 {
     //initialization of program
-    printf("File manipulation program.");
-
-    readRecord();
-
-    //initialization of file variables
-
-    //reading file
+    printf("\n\n----------File manipulation program.----------\n\n");
+    //main menu function to choose
+    mainMenu();
+    //readRecord();
 
     return 0;
+}
+
+void mainMenu()
+{
+    int userInput;
+    char menuChoice[5][20] = {
+        "Read Record",
+        "Add Record",
+        "Edit Record",
+        "Delete Record",
+        "Search"};
+    for (int i = 0; i < 5; i++)
+    {
+        printf("\t%d.", i + 1);
+        printf("%s\n", menuChoice[i]);
+    }
+    printf("\tPlease input the number of choice:");
+    scanf("%d",&userInput);
+    switch (userInput)
+    {
+    case 1:
+        printf("\nCase1: Value is: %d", userInput);
+        break;
+    case 2:
+        printf("\nCase1: Value is: %d", userInput);
+        break;
+    case 3:
+        printf("\nCase1: Value is: %d", userInput);
+        break;
+    default:
+        printf("\nDefault: Value is: %d", userInput);
+    }
 }
 
 void readRecord()
