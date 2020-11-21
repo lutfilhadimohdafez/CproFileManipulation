c2f58358d3d157acd162d684a74b6c698f8f9433
Lutfil Hadi Mohd Hafez <61901435+lutfilhadimohdafez@users.noreply.github.com>
2020-11-22 06:13:22 +0800

    Updating read and parsing csv into struct array

--- fileManipulation.c
+++ fileManipulation.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
+#include <errno.h>
 //Soalan Assignment
 /*1.Construct a database system that contains name and identity card (IC).
 This system should have 4 functions that are
@@ -14,6 +15,7 @@ The name of database file is record.csv.
 // Function prototype untuk user-defined functions
 int mainMenu();
 void readRecord();
+void printValues();
 int editRecord();
 int addRecord();
 int deleteRecord();
@@ -22,25 +24,31 @@ int searchByIC();
 int delay();
 
 //Global Variable
-FILE *studentRecord;
+FILE *customerRecord;
 
-struct student{
-    int id;
-    char name[255];
-    char nickname[255];
-    
-};
+typedef struct customerInfo
+{
+    char custId[10];
+    char custIC[255];
+    char custName[255];
+    char custContactNum[255];
+
+} custInfo;
 
 //main Program function
 int main()
 {
     //initializing the program loop variable, default set as Y(yes)
+    system("cls");
+    system("color 02");
     char continueProgram = 'Y';
     //initialization of program
-    printf("\n\n----------File manipulation program.----------\n\n");
+    
     //main menu function to choose
     do
     {
+        system("cls");
+        printf("\n\n----------Warung Makcik Bawang----------\n\n");
         mainMenu();
         printf("\n\tDo you wish to continue?(Y/N)");
         scanf(" %c", &continueProgram);
@@ -67,8 +75,12 @@ int mainMenu()
         printf("\t%d.", i + 1);
         printf("%s\n", menuChoice[i]);
     }
-    printf("\tPlease input the number of choice:");
+    printf("\n\tPlease input the number of choice:");
     scanf("%d", &userInput);
+    if (userInput < 1)
+    {
+        printf("Wrong Number");
+    }
     if (userInput >= 1 && userInput <= 6)
     {
         printf("\n\tSelected choice:%d, doing %s\n", userInput, menuChoice[userInput - 1]);
@@ -79,6 +91,7 @@ int mainMenu()
 
             delay(1);
             readRecord();
+            //printValues();
             break;
         case 2:
             //add record
@@ -117,39 +130,83 @@ int mainMenu()
         printf("\n\tWrong number choice, returning to main menu\n");
         return 0;
     }
-    else{
-        printf("\n\tInput error, returning to main menu\n");
-        return 0;
-    }
+    // else{
+    //     printf("\n\tInput error, returning to main menu\n");
+    //     return 0;
+    // }
 }
 
 void readRecord()
 {
-    studentRecord = fopen("record.csv", "r");
-    char buffer[255] = {0};
-    if (studentRecord == NULL)
+    customerRecord = fopen("record.csv", "r");
+    char buffer[1024];
+    int row_count = 0;
+    int column_count = 0;
+
+    custInfo values[100];
+
+    int i = 0;
+    while (fgets(buffer, 1024, customerRecord))
     {
-        printf("\n Failed to open file!");
-    }
-    else
-    {
-        while ((fgets(buffer, 255, studentRecord)) != NULL)
+        //printf("%s\n", buffer);//ni utk test masukkan kat dalam array
+        column_count = 0;
+        row_count++;
+        if (row_count == 1)
         {
-            puts(buffer);
-            /* Some processing */
-            //delay(1);
+            continue;
         }
-        fclose(studentRecord);
+        char *column = strtok(buffer, ",");
+        //column = malloc(15);
+        while (column)
+        {
+            if (column_count == 0)
+            {
+                strcpy(values[i].custId, column);
+            }
+            if (column_count == 1)
+            {
+                strcpy(values[i].custIC, column);
+            }
+            if (column_count == 2)
+            {
+                strcpy(values[i].custName, column);
+            }
+            if (column_count == 3)
+            {
+                strcpy(values[i].custContactNum, column);
+            }
+
+            column = strtok(NULL, ",");
+            column_count++;
+        }
+        i++;
     }
+    fclose(customerRecord);
+    printValues(values);
+    // char buffer[10] = {0};
+    // if (customerRecord == NULL)
+    // {
+    //     printf("\n Failed to open file!");
+    // }
+    // else
+    // {
+    //     while ((fgets(buffer, 255, customerRecord)) != NULL)
+    //     {
+    //         puts(buffer);
+    //         /* Some processing */
+    //         //delay(1);
+    //     }
+    //     fclose(customerRecord);
+    // }
 }
 
 int addRecord()
 {
-    char id[255],firstName[255],appName[255];
-    studentRecord = fopen("record.csv", "a");
-    fprintf(studentRecord,"");
-    fclose(studentRecord);
-    return 0;
+    // char id[255], firstName[255], appName[255];
+    // customerRecord = fopen("record.csv", "a");
+    // fprintf(customerRecord, "");
+    // fclose(customerRecord);
+    // return 0;
 }
 
 int editRecord()
@@ -185,4 +242,14 @@ int delay(int number_of_seconds)
     // looping till required time is not achieved
     while (clock() < start_time + milli_seconds)
         ;
+}
+
+void printValues(custInfo values[])
+{
+    printf("\n");
+    for (int i = 0; i < 100; i++)
+    {
+        printf("\tCust ID: %s\tCust IC: %s", values[i].custId, values[i].custIC);
+        printf("\tCust Name: %s\t\tContactNo: %s", values[i].custName, values[i].custContactNum);
+    }
 }
